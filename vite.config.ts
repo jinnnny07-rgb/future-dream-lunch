a071ebebb1c120280a97ab959bdca7a3799d2f2c
jinnnny07-rgb <jinnnny07@gmail.com>
2026-09-21 13:08:26 +0900

    fix2

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/future-dream-lunch/'
+    base: '/future-dream-lunch/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
